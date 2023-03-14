5950c4c5e5f43e138010b6f56719eef729d39763
DESKTOP-6J3G1B7\trung <truongtm@egdgroup.com>
2023-03-14 15:05:44 +0700

    demo vong doi Vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
     <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#"><a @click="redirectToPage('/')">Home page</a></b-navbar-brand>
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#"><a @click="redirectToPage('momoChargePhoneCard')">momoChargePhoneCard</a></b-nav-item>
-        <b-nav-item href="#"><a @click="redirectToPage('paymentInvoice')">paymentInvoice</a></b-nav-item>
-        <b-nav-item href="#"><a @click="redirectToPage('recharge')">recharge</a></b-nav-item>
-        <b-nav-item href="#"><a @click="redirectToPage('reportTransaction')">reportTransaction</a></b-nav-item>
-        <b-nav-item href="#"><a @click="redirectToPage('withDrawMoney')">withDrawMoney</a></b-nav-item>
-        <b-nav-item href="#"><a @click="redirectToPage('payooTransaction')">payooTransaction</a></b-nav-item>
-        <b-nav-item href="#"><a @click="redirectToPage('counter')">counter</a></b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#"><a @click="redirectToPage('/')">Home page</a></b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#"><a @click="redirectToPage('momoChargePhoneCard')">momoChargePhoneCard</a></b-nav-item>
+            <b-nav-item href="#"><a @click="redirectToPage('paymentInvoice')">paymentInvoice</a></b-nav-item>
+            <b-nav-item href="#"><a @click="redirectToPage('recharge')">recharge</a></b-nav-item>
+            <b-nav-item href="#"><a @click="redirectToPage('reportTransaction')">reportTransaction</a></b-nav-item>
+            <b-nav-item href="#"><a @click="redirectToPage('withDrawMoney')">withDrawMoney</a></b-nav-item>
+            <b-nav-item href="#"><a @click="redirectToPage('payooTransaction')">payooTransaction</a></b-nav-item>
+            <b-nav-item href="#"><a @click="redirectToPage('counter')">counter</a></b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -31,29 +31,29 @@ export default {
     redirectToPage(link) {
       switch (link) {
         case "momoChargePhoneCard":
-          this.$router.push("/momoChargePhoneCard",()=>{});
+          this.$router.push("/momoChargePhoneCard", () => { });
           break;
         case "paymentInvoice":
-          this.$router.push("/paymentInvoice",()=>{});
+          this.$router.push("/paymentInvoice", () => { });
           break;
         case "recharge":
-          this.$router.push("/recharge",()=>{});
+          this.$router.push("/recharge", () => { });
           break;
         case "reportTransaction":
-          this.$router.push("/reportTransaction",()=>{});
+          this.$router.push("/reportTransaction", () => { });
           break;
         case "withDrawMoney":
-          this.$router.push("/withDrawMoney",()=>{});
+          this.$router.push("/withDrawMoney", () => { });
           break;
         case "payooTransaction":
-          this.$router.push("/payooTransaction",()=>{});
+          this.$router.push("/payooTransaction", () => { });
           break;
         case "counter":
-          this.$router.push("/counter",()=>{});
+          this.$router.push("/counter", () => { });
           break;
 
         default:
-          this.$router.push("/",()=>{});
+          this.$router.push("/", () => { });
           break;
       }
     },
@@ -64,6 +64,7 @@ export default {
 <style lang="scss">
 @import "./styles/color.scss";
 @import "../src/styles/common.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="hello">
-    <span>Clicked: {{$store.state.count}} times, count is {{getStatus}}</span><br/>
+    <span>Clicked: {{ $store.state.count }} times, count is {{ getStatus }}</span><br />
     <button class="left-button" @click="incrementAdd(payload)">+</button>
+
     <button @click="incrementSub(payload)">-</button>
+    <p ref="usernameInput" id="someName">{{ dataTest }}</p>
   </div>
 </template>
 
@@ -15,34 +17,90 @@ export default {
     msg: String
   },
   computed: mapGetters(['getStatus']),
-  data(){
-      return{
-          payload:{
-              name:"truongtm"
-              ,age: 18
-              ,type: ""
-          }
-      }
+  data() {
+    return {
+      payload: {
+        name: "truongtm"
+        , age: 18
+        , type: ""
+      },
+      dataTest: 0
+    }
   },
   methods: {
     ...mapActions([
       'increment'
     ]),
-    incrementAdd () {
-      this.payload.type=types.INCREMENT;
+    incrementAdd() {
+      this.payload.type = types.INCREMENT;
       this.increment(this.payload);
     },
-    incrementSub () {
-      this.payload.type=types.DECREMENT;
+    incrementSub() {
+      this.payload.type = types.DECREMENT;
       this.increment(this.payload);
-    }
+    },
+
+  },
+  beforeCreate() {
+    // duoc goi khi commpent truoc khi duoc tao ra
+    console.log('before create');
+    console.log("gia tri property" + this.payload.name);
+    console.log("gia tri DOM" + document.getElementById("someName").innerHTML);
+  },
+  created() {
+    // duoc goi khi commpent da tao ra xong data va method
+    console.log('created');
+    console.log("gia tri property" + this.payload.name);
+    console.log("gia tri DOM" + document.getElementById("someName").innerHTML);
+  },
+  beforeMount() {
+    // duoc goi truoc khi component gan template vao DOM that
+    console.log('before mount')
+    console.log("gia tri property" + this.payload.name);
+    console.log("gia tri DOM" + document.getElementById("someName").innerHTML);
+  },
+  mounted() {
+    // duoc goi khi component da gan template vao DOM that
+    console.log('mounted')
+    console.log("gia tri property" + this.payload.name);
+    console.log("gia tri DOM" + document.getElementById("someName").innerHTML);
+
+    setInterval(() => {
+      this.dataTest++
+    }, 10000);
+
+    
+  },
+  beforeUpdate() {
+    // duoc goi khi doi tuong da gan vao DOM, truoc khi du lieu duoc thay doi
+    console.log("Data updated: " + this.dataTest)
+    console.log("gia tri DOM" + document.getElementById("someName").innerHTML);
+  },
+  updated() {
+    // duoc goi khi doi tuong da gan vao DOM, sau khi du lieu duoc thay doi
+    // gia tri dataTest o phase beforeUpdate va updated la giong nhau
+    console.log("Data beforeUpdate: " + this.dataTest)
+    console.log("gia tri DOM" + document.getElementById("someName").innerHTML);
+  },
+  beforeDestroy() {
+    // duoc goi truoc khi bien hoac component bi xoa
+    console.log('beforeDestroy123')
+    //  this.dataTest = null
+    //  delete this.dataTest
+  },
+  destroyed() {
+    // duoc goi sau khi bien hoac component bi xoa
+    console.log('destroyed123')
+    console.log('destroyed && content = ' + this.dataTest)
+
+    
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.left-button{
+.left-button {
   margin: 0 10px 0 0;
 }
 </style>
